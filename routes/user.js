19bd2bcdcc2360180f154e9e36d101b9525270ce
Binay <binaydas1123@gmail.com>
2024-12-12 21:30:51 +0530

    Skeleton for user routes

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const userRouter = Router(); // Instantiate the router
+
+
+userRouter.post('/signup', () => {
+
+})
+
+userRouter.post('/signin', () => {
+
+})
+
+module.exports = {
+    userRouter
+}
