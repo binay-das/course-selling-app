84138ada8252d8e954e18e363b0b7beb16388db0
Binay <binaydas1123@gmail.com>
2024-12-13 18:54:24 +0530

    User middleware

---
+++ middlewares/user.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { JWT_USER_SECRET } = require('../config');
+
+function userMiddleware(req, res, next) {
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, JWT_USER_SECRET);
+
+    if (!decoded) { 
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    req.userId = decoded.id;
+    next();
+}
+
+module.exports = {
+    userMiddleware
+};
