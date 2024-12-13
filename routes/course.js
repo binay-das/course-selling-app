b9993963f5a9324034e12ac89387398ebbdfb0ff
Binay <binaydas1123@gmail.com>
2024-12-14 00:32:02 +0530

    course router end points

--- routes/course.js
+++ routes/course.js
@@ -1,15 +1,36 @@
 const { Router } = require('express');
 const courseRouter = Router();
+const { userMiddleware } = require('../middlewares/user');
+const { purchasesModel, coursesModel } = require('../db');
 
-courseRouter.get('/preview', (req, res) => {
-    res.json({ message: 'Welcome to the course API!' });
+courseRouter.get('/preview', async (req, res) => {
+
+    try {
+        const courses = await coursesModel.find({});
+
+        res.json({ courses });
+    } catch (error) {
+        return res.status(404).json({
+            message: 'No course found',
+            error: error.message
+        });
+    }
 });
 
-courseRouter.post('/purchase', (req, res) => {
+courseRouter.post('/purchase', userMiddleware, async (req, res) => {
+    const userId = req.userId;
     const { courseId } = req.body;
     if (!courseId) {
         return res.status(400).json({ message: 'Course ID is required' });
     }
+
+    await purchasesModel.create({
+        userId,
+        courseId
+    });
+
+    res.status(200).json({ message: 'Purchase successful!' });
+
 })
 
 module.exports = {
