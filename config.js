9cddf28d7fe661946394b2caf7f0530a9db1ea18
Binay <binaydas1123@gmail.com>
2024-12-13 18:47:46 +0530

    Config

---
+++ config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const DB_URL = process.env.DB_URL;
+
+const JWT_USER_SECRET = process.env.JWT_USER_SECRET
+
+const JWT_ADMIN_SECRET = process.env.JWT_ADMIN_SECRET;
+
+
+module.exports = {
+    DB_URL,
+    JWT_USER_SECRET,
+    JWT_ADMIN_SECRET,
+};
