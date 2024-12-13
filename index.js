ee76c1c681b66de6add05009eec49f6c1d6a4b6d
Binay <binaydas1123@gmail.com>
2024-12-13 15:59:06 +0530

    Bug fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
@@ -11,18 +12,19 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1//course', courseRouter);
-app.use('/api/v1//admin', adminRouter);
+app.use('/api/v1/admin', adminRouter);
 
 async function main(req, res) {
     try {
         await mongoose.connect(process.env.DB_URL);
+        console.log('Connected to DB');
 
         const PORT = 3000;
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         })
     } catch (error) {
-        return res.status(404).json({ error: error });
+        console.log('Error:', error);
     }
 }
 
