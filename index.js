87d813bc83140b0a7418e27fc77cd1278293ebf4
Binay <binaydas1123@gmail.com>
2024-12-13 16:37:53 +0530

    json parser added

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.use(express.json());
+
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1//course', courseRouter);
 app.use('/api/v1/admin', adminRouter);
