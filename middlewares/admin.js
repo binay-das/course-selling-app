12a3716568430adf3c879fe6adfa45d7495b4edf
Binay <binaydas1123@gmail.com>
2024-12-14 17:25:13 +0530

    Auth tokens

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -2,8 +2,10 @@ const jwt = require('jsonwebtoken');
 const { JWT_ADMIN_SECRET } = require('../config');
 
 function adminMiddleware(req, res, next) {
-    const token = req.headers.token;
-    const decoded = jwt.verify(token, JWT_ADMIN_SECRET);
+    const token = req.headers.authorization;
+    const words = token.split(' ');
+    const jwtToken = words[1];
+    const decoded = jwt.verify(jwtToken, JWT_ADMIN_SECRET);
 
     if (!decoded) {
         return res.status(401).json({ message: 'Invalid token' });
--- middlewares/user.js
+++ middlewares/user.js
@@ -2,10 +2,12 @@ const jwt = require('jsonwebtoken');
 const { JWT_USER_SECRET } = require('../config');
 
 function userMiddleware(req, res, next) {
-    const token = req.headers.token;
-    const decoded = jwt.verify(token, JWT_USER_SECRET);
+    const token = req.headers.authorization;
+    const words = token.split(' ');
+    const jwtToken = words[1];
+    const decoded = jwt.verify(jwtToken, JWT_USER_SECRET);
 
-    if (!decoded) { 
+    if (!decoded) {
         return res.status(401).json({ message: 'Invalid token' });
     }
 
